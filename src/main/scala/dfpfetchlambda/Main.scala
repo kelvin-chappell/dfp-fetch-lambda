85eaecde33a3b2e4e5dd55b11239773c8ba73715
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-07-08 12:10:29 +0100

    Update report destination

--- src/main/scala/dfpfetchlambda/Main.scala
+++ src/main/scala/dfpfetchlambda/Main.scala
@@ -1,18 +1,12 @@
 package dfpfetchlambda
 
-import dfpfetchlambda.ReportQueries.{allHostedCampaigns, renaultMerchComponents}
+import dfpfetchlambda.ReportQueries.renaultMerchComponents
 
 class Main {
   def handler(): Unit = {
-
-//    S3.write(
-//      report = Dfp.fetchReport(allHostedCampaigns),
-//      dstFile = "LabsHostedCampaigns.csv"
-//    )
-
     S3.write(
       report = Dfp.fetchReport(renaultMerchComponents),
-      dstFile = "LabsRenaultMerch.csv"
+      dstFile = "LabsHostedRenaultMerch.csv"
     )
   }
 }
