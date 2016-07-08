73dee045d4235d29ef03a2a2e1efba85c7fb2c62
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-07-08 12:01:39 +0100

    Just write merch components report

--- src/main/scala/dfpfetchlambda/Main.scala
+++ src/main/scala/dfpfetchlambda/Main.scala
@@ -5,10 +5,10 @@ import dfpfetchlambda.ReportQueries.{allHostedCampaigns, renaultMerchComponents}
 class Main {
   def handler(): Unit = {
 
-    S3.write(
-      report = Dfp.fetchReport(allHostedCampaigns),
-      dstFile = "LabsHostedCampaigns.csv"
-    )
+//    S3.write(
+//      report = Dfp.fetchReport(allHostedCampaigns),
+//      dstFile = "LabsHostedCampaigns.csv"
+//    )
 
     S3.write(
       report = Dfp.fetchReport(renaultMerchComponents),
